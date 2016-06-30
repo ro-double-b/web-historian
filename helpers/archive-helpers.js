57befaa44b1ccab8523d0b6158018b6afa52129f
ro-double-b <robb.marsee@gmail.com>
2016-06-30 16:31:58 -0700

    Add file to previous commit.

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+//test
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -11,8 +11,8 @@ var _ = require('underscore');
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
-  archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  archivedSites: path.join(__dirname, '../web/archives/sites'),
+  list: path.join(__dirname, '../web/archives/sites.txt')
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -25,20 +25,25 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(cb) {
+exports.readListOfUrls = function(cb2) {
   fs.readFile(exports.paths.list, 'utf8', function(err, data) {
     if (err) {
-      console.log('failed to read file', data);
+      console.log('failed to read file', data, err);
     } else {
       console.log('able to read file', data);
-      cb(data.split('\n'));
+      cb2(data.split('\n'));
     }
   });
 };
 
-exports.isUrlInList = function(urlRequested, cb) {
+exports.isUrlInList = function(urlRequested, cb1, failed) {
   return exports.readListOfUrls( function(list) {
-    cb(list.indexOf(urlRequested) !== -1);
+    if (list.indexOf(urlRequested) !== -1) {
+      cb1();
+    } else {
+      failed();
+      return false;
+    }
   });
 };
 
