f2c47808d58b70c168e85b0cb4b042c1cfb94a7a
ro-double-b <robb.marsee@gmail.com>
2016-06-30 19:16:31 -0700

    Successfully fetches html data from non https web addresses and stores in an archive folder.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,23 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var path = require('path');
+var fs = require('fs');
+var archive = require('../helpers/archive-helpers');
+var _ = require('underscore');
+
+var totalUrls = [];
+var notArchivedUrls = [];
+
+var makeToArchiveList = () => {
+  totalUrls.forEach(function(url) {
+    archive.isUrlArchived(url, _.identity, (urlNotArch) => {
+      notArchivedUrls.push(urlNotArch);
+      archive.downloadUrls(notArchivedUrls);
+    });
+  });
+  
+};
+
+archive.readListOfUrls((list) => {
+  totalUrls = list;
+}, makeToArchiveList);
